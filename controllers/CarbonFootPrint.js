1bd9185c0f1a7df20e7dc0f41a21e2cb0aaab5f5
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-12-12 16:28:39 +0300

    crate carbon foot print

---
+++ controllers/CarbonFootPrint.js
@@ -0,0 +1,22 @@
+const CarbonFootPrint = require("../models/CarbonFootPrint");
+
+const createCarbonFootprint = async (req, res) => {
+  try {
+    const data = req.body;
+    const carbonFootprint = new CarbonFootPrint(data);
+    const savedEntry = await carbonFootprint.save();
+    res.status(201).json({
+      message: "Carbon footprint created successfully",
+      data: savedEntry,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to create carbon footprint",
+      error: error.message,
+    });
+  }
+};
+
+module.exports({
+  createCarbonFootprint,
+});
