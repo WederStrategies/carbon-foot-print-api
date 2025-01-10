2e05b658a4eae6af2f5000723374159d290d33d4
teklehaimanot <teklehaimanotbelete@gmail.com>
2025-01-10 11:39:42 +0300

    Added Api's for carbon foot print reports

--- reports/carbonFootprint.routes.js
+++ reports/carbonFootprint.routes.js
@@ -2,13 +2,23 @@ const express = require("express");
 const {
   getSummary,
   listDietAndFoodResults,
-  listWasteDisposalResults,
+  getWeeklyWasteDispoalFrequency,
+  getRecycleMaterialCount,
+  getHouseHoldUsage,
+  getTransportationModeUsage,
+  getTotalWaterUsage,
+  getAverageWaterUsageFrequency,
 } = require("./carbonFootprint.logic");
 
 const carbonFootprintRouter = express.Router();
 
 carbonFootprintRouter.get("/summary", getSummary);
 carbonFootprintRouter.get("/dietAndFood", listDietAndFoodResults);
-carbonFootprintRouter.get("/wasteDisposal", listWasteDisposalResults);
+carbonFootprintRouter.get("/wasteDisposal", getWeeklyWasteDispoalFrequency);
+carbonFootprintRouter.get("/recycleMaterial", getRecycleMaterialCount);
+carbonFootprintRouter.get("/houseHoldusage", getHouseHoldUsage);
+carbonFootprintRouter.get("/transportationMode", getTransportationModeUsage);
+carbonFootprintRouter.get("/WaterUsage", getTotalWaterUsage);
+carbonFootprintRouter.get("/WaterUsageDuration", getAverageWaterUsageFrequency);
 
 module.exports = carbonFootprintRouter;
